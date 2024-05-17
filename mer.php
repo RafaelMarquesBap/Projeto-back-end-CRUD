a31c6d2b737ad6f1f64f49d21fddc5a4f96fb5e2
Rafael <rafael.rmarquea@gmail.com>
2024-05-17 08:26:47 -0300

    Tirei session start duplicada

--- mer.php
+++ mer.php
@@ -22,7 +22,7 @@ print_r($_SESSION);
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="imagens/iconeR(1).jpg" type="image/x-icon" />
-    <title>O Rafaelo</title>
+    <title>O Rafaelo - Mer</title>
   </head>
 
   <body>
